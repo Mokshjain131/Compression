4dc13a5bb0dcb8cec70513366367c6b093f08f1b
Mokshjain131 <mokshjain131@gmail.com>
2025-04-21 11:22:01 +0530

    rle decoding

--- src/rle.cpp
+++ src/rle.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 string run_length_encoding(string s);
+string run_length_decoding(string s);
 
 int main() {
     string s = "hhhkskkseeddtjooooodnnnkkkshhh";
-    string result = run_length_encoding(s);
     cout << "Original string: " << s << endl;
-    cout << "Encoded string: " << result << endl;
+
+    string encoded_string = run_length_encoding(s);
+    cout << "Encoded string: " << encoded_string << endl;
+    cout << "Length of encoded string: " << encoded_string.length() << endl;
+
+    string decoded_string = run_length_decoding(encoded_string);
+    cout << "Decoded string: " << decoded_string << endl;
+    cout << "Length of original string: " << s.length() << endl;
+
 }
 
 string run_length_encoding(string s) {
@@ -24,6 +32,22 @@ string run_length_encoding(string s) {
         }
         result += to_string(count);
         i++;
+    }
+
+    return result;
+}
+
+string run_length_decoding(string s) {
+    string result = "";
+    int len = s.length();
+    if (len == 0) return result;
+    int i = 1;
+
+    while (i < len) {
+        for (int j = 0; j < (s[i] - '0'); j++) {
+            result += s[i - 1];
+        }
+        i += 2;
     }
 
     return result;
